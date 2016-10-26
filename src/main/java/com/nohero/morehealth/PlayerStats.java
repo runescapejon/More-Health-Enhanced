2186f379716f105ff6a8153f604aa8191a58aed1
runescapejon <jonsimpletv@gmail.com>
2016-10-26 05:44:54 -0400

    Update PlayerStats.java

--- src/main/java/com/nohero/morehealth/PlayerStats.java
+++ src/main/java/com/nohero/morehealth/PlayerStats.java
@@ -1,37 +1,34 @@
 package com.nohero.morehealth;
 
 import com.nohero.morehealth.EventHandlers.PlayerHandler;
+import java.util.concurrent.ConcurrentHashMap;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
-public class PlayerStats {
-	
-	//public WeakReference<EntityPlayer> player;
-	//public int level;
-
-	public  int[] LevelArray;
-	public  int start;
-	public  int hpmax; //tracks max health of each player -- seems to be unused now
-	public  int count; //tracks current position in LevelRamp array.
-	public 	int previousLevel; //previous exp lvl of player.
-	public double healthmod; //used when a player's health modifier is lost (on respawn or login or change dim)
-	public EntityPlayer player; //the player we are referring to
-	public boolean needClientSideHealthUpdate = false;
-	public ItemStack[] oldArmorSet = new ItemStack[4];	//the armor equipped on user when stats are saved
-	public boolean justLoggedIn;
-	public float loggedOutHealth;
-	public int heartContainers; //tracks how many heartContainers used
-
-	public static PlayerStats getPlayerStats(String username) {
-		// TODO Auto-generated method stub
-		PlayerStats stats = PlayerHandler.playerStats.get(username);
-		if(stats==null)
-		{
-			stats=new PlayerStats();
-			{
-				PlayerHandler.playerStats.put(username,stats);
-			}
-		}
-		return stats;
-	}
+public class PlayerStats
+{
+  public int[] LevelArray;
+  public int start;
+  public int hpmax;
+  public int count;
+  public int previousLevel;
+  public double healthmod;
+  public EntityPlayer player;
+  public boolean needClientSideHealthUpdate = false;
+  public ItemStack[] oldArmorSet = new ItemStack[4];
+  public boolean justLoggedIn;
+  public float loggedOutHealth;
+  public int heartContainers;
+  
+  public static PlayerStats getPlayerStats(String username)
+  {
+    PlayerStats stats = (PlayerStats)PlayerHandler.playerStats.get(username);
+    if (stats == null)
+    {
+      stats = new PlayerStats();
+      
+      PlayerHandler.playerStats.put(username, stats);
+    }
+    return stats;
+  }
 }
