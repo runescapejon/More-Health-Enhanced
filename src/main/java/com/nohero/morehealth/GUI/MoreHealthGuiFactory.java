38c6a19d0fc1b0092135fb9ce0dd51e161e8af14
runescapejon <jonsimpletv@gmail.com>
2016-10-26 05:56:04 -0400

    Update MoreHealthGuiFactory.java

--- src/main/java/com/nohero/morehealth/GUI/MoreHealthGuiFactory.java
+++ src/main/java/com/nohero/morehealth/GUI/MoreHealthGuiFactory.java
@@ -1,55 +1,41 @@
 package com.nohero.morehealth.GUI;
 
-import com.nohero.morehealth.mod_moreHealthEnhanced;
-import cpw.mods.fml.client.IModGuiFactory;
-import cpw.mods.fml.client.IModGuiFactory.RuntimeOptionCategoryElement;
-import cpw.mods.fml.client.IModGuiFactory.RuntimeOptionGuiHandler;
-import cpw.mods.fml.client.config.GuiConfig;
-import cpw.mods.fml.client.config.IConfigElement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraftforge.common.config.ConfigElement;
-import net.minecraftforge.common.config.Configuration;
+import cpw.mods.fml.client.*;
+import net.minecraft.client.*;
+import net.minecraft.client.gui.*;
+import com.nohero.morehealth.*;
+import cpw.mods.fml.client.config.*;
+import net.minecraftforge.common.config.*;
+import java.util.*;
 
-public class MoreHealthGuiFactory
-  implements IModGuiFactory
+public class MoreHealthGuiFactory implements IModGuiFactory
 {
-  public void initialize(Minecraft minecraftInstance) {}
-  
-  public Class<? extends GuiScreen> mainConfigGuiClass()
-  {
-    return ModConfigGUI.class;
-  }
-  
-  public Set<IModGuiFactory.RuntimeOptionCategoryElement> runtimeGuiCategories()
-  {
-    return null;
-  }
-  
-  public IModGuiFactory.RuntimeOptionGuiHandler getHandlerFor(IModGuiFactory.RuntimeOptionCategoryElement element)
-  {
-    return null;
-  }
-  
-  public static class ModConfigGUI
-    extends GuiConfig
-  {
-    public ModConfigGUI(GuiScreen parent)
-    {
-      super(
-        getConfigElements(), "morehealth", false, false, 
-        GuiConfig.getAbridgedConfigPath(mod_moreHealthEnhanced.config.toString()));
+    public void initialize(final Minecraft minecraftInstance) {
     }
     
-    private static List<IConfigElement> getConfigElements()
-    {
-      List<IConfigElement> list = new ArrayList();
-      list.addAll(new ConfigElement(mod_moreHealthEnhanced.config.getCategory("general")).getChildElements());
-      list.addAll(new ConfigElement(mod_moreHealthEnhanced.config.getCategory("gui options")).getChildElements());
-      return list;
+    public Class<? extends GuiScreen> mainConfigGuiClass() {
+        return (Class<? extends GuiScreen>)ModConfigGUI.class;
     }
-  }
+    
+    public Set<IModGuiFactory.RuntimeOptionCategoryElement> runtimeGuiCategories() {
+        return null;
+    }
+    
+    public IModGuiFactory.RuntimeOptionGuiHandler getHandlerFor(final IModGuiFactory.RuntimeOptionCategoryElement element) {
+        return null;
+    }
+    
+    public static class ModConfigGUI extends GuiConfig
+    {
+        public ModConfigGUI(final GuiScreen parent) {
+            super(parent, (List)getConfigElements(), "morehealth", false, false, GuiConfig.getAbridgedConfigPath(mod_moreHealthEnhanced.config.toString()));
+        }
+        
+        private static List<IConfigElement> getConfigElements() {
+            final List<IConfigElement> list = new ArrayList<IConfigElement>();
+            list.addAll(new ConfigElement(mod_moreHealthEnhanced.config.getCategory("general")).getChildElements());
+            list.addAll(new ConfigElement(mod_moreHealthEnhanced.config.getCategory("gui options")).getChildElements());
+            return list;
+        }
+    }
 }
